a3d6170fce6686c5a9a77a5ea2bbc984113ec6df
Vincent Michael Bacay <vmtbacay@gmail.com>
2021-05-19 18:28:17 +0000

    changed how profile pics work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    @user.image.attach(params[:user][:image])
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 80 })
-    size = options[:size]
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  def gravatar_for(user, options = { size: 50 })
+    if user.image.attached?
+      gravatar_url = user.image.variant(resize_to_limit: [options[:size], options[:size]])
+    else
+      gravatar_url = 'default_profile_pic.jpg'
+    end
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
