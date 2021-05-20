87f661e8bbe385ad31f61fff307cbd785c8b44f9
Vincent Michael Bacay <vmtbacay@gmail.com>
2021-05-20 10:19:42 +0000

    Fixed image sizes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 50 })
+  def gravatar_for(user, options = { size: 80 })
     if user.image.attached?
       gravatar_url = user.image.variant(resize_to_limit: [options[:size], options[:size]])
     else
